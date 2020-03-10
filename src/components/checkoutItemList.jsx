d21bd15d9baf090dbea8c5978d813714642c4f71
Makenna <makennasmutz@gmail.com>
2020-03-10 23:05:44 +0100

    fix checkout styling

--- src/components/checkoutItemList.jsx
+++ src/components/checkoutItemList.jsx
@@ -2,7 +2,14 @@ import React, { useContext } from "react";
 import { useCart } from "react-use-cart";
 import CheckoutItem from "./checkoutItem";
 import CheckoutContext from "../context/checkout";
-import { StatNumber, StatLabel, Stat, Divider, Stack } from "@chakra-ui/core";
+import {
+  StatNumber,
+  StatLabel,
+  Stat,
+  Divider,
+  Stack,
+  Box
+} from "@chakra-ui/core";
 
 function CheckoutItemList() {
   const { items, cartTotal } = useCart();
@@ -15,32 +22,33 @@ function CheckoutItemList() {
     }).format(value / 100);
 
   return (
-    <Stack
-      backgroundColor="gray.900"
-      p={4}
-      borderRadius="md"
-      spacing={4}
-      color="white"
-    >
-      {items.map(CheckoutItem)}
-      <Stat>
-        <StatLabel>Subtotal</StatLabel>
-        <StatNumber>{formatValue(cartTotal)}</StatNumber>
-      </Stat>
-      <Stat>
-        <StatLabel>Tax</StatLabel>
-        <StatNumber>{tax ? formatValue(tax) : "-"}</StatNumber>
-      </Stat>
-      <Stat>
-        <StatLabel>Shipping</StatLabel>
-        <StatNumber>{shipping ? formatValue(shipping) : "-"}</StatNumber>
-      </Stat>
-      <Divider borderColor="gray.800" />
-      <Stat>
-        <StatLabel>Total</StatLabel>
-        <StatNumber>{formatValue(orderTotal)}</StatNumber>
-      </Stat>
-    </Stack>
+    <Box>
+      <Stack
+        backgroundColor='gray.900'
+        p={4}
+        borderRadius='md'
+        spacing={4}
+        color='white'>
+        {items.map(CheckoutItem)}
+        <Stat mt={8}>
+          <StatLabel>Subtotal</StatLabel>
+          <StatNumber>{formatValue(cartTotal)}</StatNumber>
+        </Stat>
+        <Stat>
+          <StatLabel>Tax</StatLabel>
+          <StatNumber>{tax ? formatValue(tax) : "-"}</StatNumber>
+        </Stat>
+        <Stat>
+          <StatLabel>Shipping</StatLabel>
+          <StatNumber>{shipping ? formatValue(shipping) : "-"}</StatNumber>
+        </Stat>
+        <Divider borderColor='gray.800' />
+        <Stat>
+          <StatLabel>Total</StatLabel>
+          <StatNumber>{formatValue(orderTotal)}</StatNumber>
+        </Stat>
+      </Stack>
+    </Box>
   );
 }
 
