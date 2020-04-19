7cb6d769d99c3d890bf1e761bbd9cb97dfa4bd35
Makenna Smutz <makennasmutz@gmail.com>
2020-04-19 16:42:16 +0200

    update to live stripe keys and remove test info

--- src/components/CheckoutForm/paymentInfo.jsx
+++ src/components/CheckoutForm/paymentInfo.jsx
@@ -1,16 +1,8 @@
 import React from "react";
-import { Code, Text } from "@chakra-ui/core";
+import { Text } from "@chakra-ui/core";
 
 function PaymentInfo() {
-  return (
-    <Text>
-      This is a <Text as='strong'>test checkout</Text>. You can simulate
-      transactions using any valid expiry date, CVC code and{" "}
-      <Code variantColor='blue'>4242 4242 4242 4242</Code>, or{" "}
-      <Code variantColor='blue'>4000 0000 0000 3220</Code> if you want trigger
-      3D Secure 2 authentication.
-    </Text>
-  );
+  return <Text>Input your payment information.</Text>;
 }
 
 export default PaymentInfo;
